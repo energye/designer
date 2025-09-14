620f75c4f43edc3c9768285d821cbfaae5ced98d
yanghy <snxamdf@126.com>
2025-09-14 20:18:21 +0800

    设计器窗口

--- designer/designer.go
+++ designer/designer.go
@@ -11,23 +11,42 @@ import (
 func (m *TAppWindow) createDesignerLayout() {
 	// 顶部菜单
 	m.createMenu()
+	// 工具栏
+	m.createToolbar()
 }
 
+// 顶部菜单
 func (m *TAppWindow) createMenu() {
 	m.mainMenu = lcl.NewMainMenu(m)
 	file := lcl.NewMenuItem(m)
 	file.SetCaption("文件(&F)")
-	fileCreate := lcl.NewMenuItem(m)
-	fileCreate.SetCaption("新建(&N)")
-	fileCreate.SetShortCut(api.TextToShortCut("Ctrl+N"))
-	fileCreate.SetOnClick(func(lcl.IObject) {
-		fmt.Println("单击了新建")
-	})
-	file.Add(fileCreate)
-
 	m.mainMenu.Items().Add(file)
+	fileCreateWindow := lcl.NewMenuItem(m)
+	fileCreateWindow.SetCaption("新建窗体(&N)")
+	fileCreateWindow.SetShortCut(api.TextToShortCut("Ctrl+N"))
+	fileCreateWindow.SetOnClick(func(lcl.IObject) {
+		fmt.Println("单击了新建窗体")
+	})
+	file.Add(fileCreateWindow)
+
+	edit := lcl.NewMenuItem(m)
+	edit.SetCaption("编辑(&E)")
+	m.mainMenu.Items().Add(edit)
+
+	setting := lcl.NewMenuItem(m)
+	setting.SetCaption("设置(&S)")
+	m.mainMenu.Items().Add(setting)
+
+	helper := lcl.NewMenuItem(m)
+	helper.SetCaption("帮助(&H)")
+	m.mainMenu.Items().Add(helper)
 
 	if tool.IsDarwin() {
 
 	}
+}
+
+// 创建工具栏
+func (m *TAppWindow) createToolbar() {
+
 }
