83a573be469d1ed1ac63c2fbece4392e0b06009a
yanghy <snxamdf@126.com>
2025-09-18 22:04:18 +0800

    ENERGY designer

--- designer/desinger_component.go
+++ designer/desinger_component.go
@@ -1,6 +1,7 @@
 package designer
 
 import (
+	"fmt"
 	"github.com/energye/lcl/lcl"
 	"github.com/energye/lcl/types"
 )
@@ -14,7 +15,10 @@ type DesigningComponent struct {
 }
 
 func (m *DesigningComponent) OnMouseMove(sender lcl.IObject, shift types.TShiftState, X int32, Y int32) {
-
+	hint := fmt.Sprintf(`%v: %v
+	Left: %v Top: %v
+	Width: %v Height: %v`, m.object.Caption(), m.object.ToString(), m.object.Left(), m.object.Top(), m.object.Width(), m.object.Height())
+	m.object.SetHint(hint)
 }
 
 func (m *DesigningComponent) OnMouseDown(sender lcl.IObject, button types.TMouseButton, shift types.TShiftState, X int32, Y int32) {
@@ -36,9 +40,14 @@ func NewButtonDesigner(designerForm *FormTab, x, y int32) *DesigningComponent {
 	comp.SetTop(y)
 	comp.SetCursor(types.CrSize)
 	comp.SetCaption(designerForm.GetComponentCaptionName("Button"))
+	comp.SetShowHint(true)
 	comp.SetOnMouseMove(m.OnMouseMove)
 	comp.SetOnMouseDown(m.OnMouseDown)
 	comp.SetOnMouseUp(m.OnMouseUp)
+	comp.SetOnShowHint(func(sender lcl.IObject, hintInfo lcl.THintInfo) {
+		//fmt.Printf("SetOnShowHint: %+v\n", hintInfo)
+		fmt.Println("SetOnShowHint:", hintInfo.HintStr)
+	})
 	m.drag = newDrag(designerForm.designerBox, DsAll)
 	m.drag.SetRelation(comp)
 	m.object = lcl.AsWinControl(comp)
@@ -56,6 +65,7 @@ func NewEditDesigner(designerForm *FormTab, x, y int32) *DesigningComponent {
 	comp.SetCursor(types.CrSize)
 	comp.SetCaption(designerForm.GetComponentCaptionName("Edit"))
 	comp.SetText(comp.Caption())
+	comp.SetShowHint(true)
 	comp.SetOnMouseMove(m.OnMouseMove)
 	comp.SetOnMouseDown(m.OnMouseDown)
 	comp.SetOnMouseUp(m.OnMouseUp)
