afbe6b0acee34e945cd928187ea881101bb841d0
yanghy <snxamdf@126.com>
2025-09-19 12:12:29 +0800

    ENERGY designer

--- pkg/tool/tool.go
+++ pkg/tool/tool.go
@@ -6,11 +6,19 @@ import (
 )
 
 func ImageListAddPng(imageList lcl.IImageList, filePath string) {
-	if filePath == "" {
-		return
-	}
 	data := resources.Images(filePath)
 	if data != nil {
+		pic := lcl.NewPicture()
+		defer pic.Free()
+
+		mem := lcl.NewMemoryStream()
+		defer mem.Free()
+		lcl.StreamHelper.WriteBuffer(mem, data)
+		mem.SetPosition(0)
+		pic.LoadFromStream(mem)
+		imageList.Add(pic.Bitmap(), nil)
+	} else {
+		data = resources.Images("components/default_150.png")
 		pic := lcl.NewPicture()
 		defer pic.Free()
 
