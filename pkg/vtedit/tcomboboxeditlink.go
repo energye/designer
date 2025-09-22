ed6c183be2fddbcedd5ceb0b1052d45b0cfb2c53
yanghy <snxamdf@126.com>
2025-09-22 20:24:33 +0800

    ENERGY designer

--- pkg/vtedit/tcomboboxeditlink.go
+++ pkg/vtedit/tcomboboxeditlink.go
@@ -1,3 +1,27 @@
 package vtedit
 
+import (
+	"github.com/energye/lcl/lcl"
+	"github.com/energye/lcl/types"
+)
+
 // 下拉框
+
+type IComboBoxEditLink interface {
+	lcl.ICustomVTEditLink
+	AsIVTEditLink() lcl.IVTEditLink
+	SetOnNewData(fn TOnNewData)
+}
+
+type TComboBoxEditLink struct {
+	lcl.ICustomVTEditLink
+	newData    TOnNewData
+	edit       lcl.IComboBox
+	tree       lcl.ILazVirtualStringTree
+	node       types.PVirtualNode
+	column     int32
+	textBounds types.TRect
+	text       string
+	alignment  types.TAlignment
+	stopping   bool
+}
