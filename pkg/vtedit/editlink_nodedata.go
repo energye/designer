53797e1dd753047adac28f4b5b66e2d9c78ad9f5
yanghy <snxamdf@126.com>
2025-09-25 20:29:34 +0800

    ENERGY designer

--- pkg/vtedit/editlink_nodedata.go
+++ pkg/vtedit/editlink_nodedata.go
@@ -28,8 +28,9 @@ type TEditLinkNodeData struct {
 	Index         int32                // 值索引 值是数组类型时，选中的索引
 	Checked       bool                 // 选中列表 值是数组类型时，是否选中
 	StringValue   string               // 属性值 string
-	DoubleValue   float64              // 属性值 double
-	BoolValue     bool                 // 属性值 boolean
+	FloatValue    float64              // 属性值 float64
+	BoolValue     bool                 // 属性值 bool
+	IntValue      int                  // 属性值 int
 	CheckBoxValue []*TEditLinkNodeData // 属性值 checkbox
 	ComboBoxValue []*TEditLinkNodeData // 属性值 combobox
 	Type          PropertyDataType     // 属性值类型 普通文本, 单选框, 多选框, 下拉框, 菜单(子菜单)
