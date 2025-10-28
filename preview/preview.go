ea1fd972ed7635a54f42ea3327cdd327262ec1c7
yanghy <snxamdf@126.com>
2025-10-28 14:17:55 +0800

    ENERGY designer v0.0.68 ui go 生成

--- preview/preview.go
+++ preview/preview.go
@@ -2,4 +2,4 @@ package preview
 
 // 预览设计窗体
 // 触发设计器中的预览功能
-// 依赖 uigen UI 布局文件
+// 运行生成的Go代码
--- uigen/uigen.go
+++ uigen/uigen.go
@@ -1,4 +1,4 @@
 package uigen
 
 // UI 布局文件生成, JSON 格式, 自动时时生成
-// 依赖 designer
+// 依赖 designer 生成 JSON UI文件(form[n].ui)
