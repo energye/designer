c5421644104689f9b9bc86d89b7f7f19c34bdeb1
yanghy <snxamdf@126.com>
2025-10-28 14:24:33 +0800

    ENERGY designer v0.0.68 ui go 生成

--- codegen/codegen.go
+++ codegen/codegen.go
@@ -2,3 +2,4 @@ package codegen
 
 // go 代码生成 自动时时生成
 // 依赖 uigen UI 布局文件
+// 生成触发条件: 检查文件修改
--- uigen/uigen.go
+++ uigen/uigen.go
@@ -2,3 +2,6 @@ package uigen
 
 // UI 布局文件生成, JSON 格式, 自动时时生成
 // 依赖 designer 生成 JSON UI文件(form[n].ui)
+// 只存放被修改的组件属性
+// xxx.ui 文件内容是 tree JSON 结构, 数据格式为组件[变更的属性列表]
+// 生成触发条件:
