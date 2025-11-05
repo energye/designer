532cd8789e5338c2b49060dbe79f72846897ad60
yanghy <snxamdf@126.com>
2025-11-05 20:52:40 +0800

    优化事件处理: (生成UI事件 生成代码事件 项目配置事件 预览事件 控制台事件)

---
+++ event/bean.go
@@ -0,0 +1,20 @@
+// Copyright © yanghy. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License.
+
+package event
+
+// TPayload 通用的事件数据载体
+type TPayload struct {
+	Type int
+	Data any
+}
