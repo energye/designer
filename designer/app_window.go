5ba6d08bc954a6951006e4d7148f84952a5564dd
yanghy <snxamdf@126.com>
2025-11-10 16:09:41 +0800

    优化设置颜色逻辑

--- designer/app_window.go
+++ designer/app_window.go
@@ -85,7 +85,7 @@ func (m *TAppWindow) FormCreate(sender lcl.IObject) {
 	m.showInMonitor()
 	m.initAllImageList()
 	// 设置窗口图标
-	m.setWindowIcon()
+	//m.setWindowIcon()
 	// 窗口显示事件
 	m.SetOnShow(m.OnShow)
 	// 创建设计器布局
