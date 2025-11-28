1e3cd1461d37b6566d16af04117c9230bf31d209
yanghy <snxamdf@126.com>
2025-11-28 20:56:37 +0800

    增加-设计器在没有设计界面时显示一个提示性文字

--- designer/bottom_designer.go
+++ designer/bottom_designer.go
@@ -37,7 +37,9 @@ type Designer struct {
 	tab           *wg.TTab         // 设计器 tabs
 	tabMenu       lcl.IPopupMenu   // tab 菜单
 	designerForms map[int]*FormTab // 设计器窗体列表
-	designerCount int              // 设计器窗总数，动态更新
+	designerCount int              // 设计器窗总数，值动态更新
+	// 默认提示
+	defaultTip *wg.TButton
 }
 
 // 创建设计器的布局
@@ -59,6 +61,27 @@ func (m *BottomBox) createFromDesignerLayout() *Designer {
 	des.tab.ScrollRight().SetColor(wg.DarkenColor(bgLightColor, 0.1))
 	des.tab.EnableScrollButton(false)
 	des.tab.SetParent(m.rightBox)
+
+	des.defaultTip = wg.NewButton(des.tab)
+	des.defaultTip.SetDisabledColor(colors.RGBToColor(204, 232, 255), colors.RGBToColor(204, 232, 255))
+	defaultTipBr := des.defaultTip.BoundsRect()
+	dtw, dth := int32(140), int32(140)
+	defaultTipBr.Left = m.rightBox.Width()/2 - dtw/2
+	defaultTipBr.Top = m.rightBox.Height()/2 - dth/2
+	defaultTipBr.SetWidth(dtw)
+	defaultTipBr.SetHeight(dth)
+	des.defaultTip.SetBoundsRect(defaultTipBr)
+	des.defaultTip.SetAlpha(80)
+	des.defaultTip.SetRadius(15)
+	des.defaultTip.TextAlign = wg.TextAlignLeft
+	des.defaultTip.TextLineSpacing = 8
+	des.defaultTip.TextOffSetX = 10
+	des.defaultTip.Font().SetSize(10)
+	des.defaultTip.Font().SetColor(wg.DarkenColor(colors.ClGray, 0.2))
+	des.defaultTip.SetDisable(true)
+	des.defaultTip.SetAnchors(types.NewSet())
+	des.defaultTip.SetText("新建项目 (Ctrl+P)\n打开项目 (Ctrl+O)\n新建窗体 (Ctrl+N)\n应用配置 (Ctrl+F11)\n　　运行 (F9)")
+	des.defaultTip.SetParent(des.tab)
 
 	// 创建tab上的右键菜单
 	des.createTabMenu()
