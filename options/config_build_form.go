a581fb2669dadd3848decdc5461d42def42bbae9
yanghy <snxamdf@126.com>
2025-12-16 21:07:48 +0800

    构建配置

--- options/config_build_form.go
+++ options/config_build_form.go
@@ -42,9 +42,17 @@ type TBuildForm struct {
 	font      lcl.IFont
 	selectDir lcl.ISelectDirectoryDialog
 
+	buildTab            *wg.TTab
+	buildTabPageConfig  *wg.TPage
+	buildTabPagePackage *wg.TPage
+
+	// 基础配置
+
+	// 构建打包
+
 	// 操作按钮
-	cancelBtn *wg.TButton
-	saveBtn   *wg.TButton
+	saveBtn  *wg.TButton
+	buildBtn *wg.TButton
 }
 
 func (m *TBuildForm) FormCreate(sender lcl.IObject) {
@@ -71,38 +79,110 @@ func (m *TBuildForm) FormCreate(sender lcl.IObject) {
 	m.SetColor(colors.ClWhite)
 
 	m.selectDir = lcl.NewSelectDirectoryDialog(m)
-	{
 
+	{
+		m.buildTab = wg.NewTab(m)
+		m.buildTab.Margin = 5
+		tabBR := types.TRect{Left: 0, Top: 5}
+		tabBR.SetWidth(m.Width())
+		tabBR.SetHeight(m.Height() - tabBR.Top)
+		m.buildTab.SetBoundsRect(tabBR)
+		m.buildTab.SetColor(colors.ClWhite)
+		m.buildTab.EnableScrollButton(false)
+		m.buildTab.SetParent(m)
+		m.buildTab.SetOnChange(func(sender lcl.IObject) {
+			//for _, page := range m.buildTab.Pages() {
+			//	if page.Active() {
+			//		page.Button().SetBorderDirections(0)
+			//		page.Button().Font().SetColor(colors.ClWhite)
+			//		page.Button().SetIconFavoriteFormBytes(buttons.Get(page.Button().Text()).iconActive)
+			//	} else {
+			//		page.Button().SetBorderDirections(types.NewSet(wg.BbdBottom, wg.BbdLeft, wg.BbdTop, wg.BbdRight))
+			//		page.Button().Font().SetColor(colors.ClBlack)
+			//		page.Button().SetIconFavoriteFormBytes(buttons.Get(page.Button().Text()).iconDefault)
+			//	}
+			//}
+		})
+
+		m.buildTabPageConfig = m.buildTab.NewPage()
+		m.buildTabPageConfig.SetCaption("基础配置")
+		//m.buildTabPageConfig.Button().SetIconFavoriteFormBytes(buttons.Get("Windows").iconDefault)
+		//setTabPageStyle(m.platformTabPageWindows)
+
+		m.buildTabPagePackage = m.buildTab.NewPage()
+		m.buildTabPagePackage.SetCaption("构建打包")
+		//m.buildTabPageConfig.Button().SetIconFavoriteFormBytes(buttons.Get("Windows").iconDefault)
+		//setTabPageStyle(m.platformTabPageWindows)
+
+		m.buildTabPageConfig.SetActive(true)
 	}
 
+	m.initConfigComponent()
+	m.initBuildComponent()
+
 	{
-		m.cancelBtn = wg.NewButton(m)
-		m.cancelBtn.SetText("关　闭")
-		m.cancelBtn.SetFont(m.font)
-		m.cancelBtn.Font().SetColor(colors.ClWhite)
-		m.cancelBtn.SetRadius(3)
-		cancelBtnRect := types.TRect{Left: 315, Top: buildFormHeight - 45}
-		cancelBtnRect.SetWidth(100)
-		cancelBtnRect.SetHeight(35)
-		m.cancelBtn.SetBoundsRect(cancelBtnRect)
-		m.cancelBtn.SetColor(colors.RGBToColor(255, 127, 127))
-		m.cancelBtn.SetParent(m)
-		m.cancelBtn.SetOnClick(m.closeClick)
+		btnFont := lcl.NewFont()
+		btnFont.SetName("微软雅黑")
+		btnFont.SetSize(10)
+		btnFont.SetStyle(types.NewSet(types.FsBold))
 
 		m.saveBtn = wg.NewButton(m)
-		m.saveBtn.SetText("保　存")
-		m.saveBtn.SetFont(m.font)
+		m.saveBtn.SetText("保存配置")
+		m.saveBtn.SetFont(btnFont)
 		m.saveBtn.Font().SetColor(colors.ClWhite)
 		m.saveBtn.SetRadius(3)
-		saveBtnRect := types.TRect{Left: cancelBtnRect.Left + cancelBtnRect.Width() + 30, Top: cancelBtnRect.Top}
-		saveBtnRect.SetWidth(100)
-		saveBtnRect.SetHeight(35)
+		saveBtnRect := types.TRect{Left: 390, Top: 0}
+		saveBtnRect.SetWidth(75)
+		saveBtnRect.SetHeight(25)
 		m.saveBtn.SetBoundsRect(saveBtnRect)
-		m.saveBtn.SetColor(colors.RGBToColor(46, 204, 113))
-		m.saveBtn.SetParent(m)
+		m.saveBtn.SetColor(colors.RGBToColor(59, 130, 246))
+		m.saveBtn.SetParent(m.buildTab)
 		m.saveBtn.SetOnClick(m.saveClick)
+
+		m.buildBtn = wg.NewButton(m)
+		m.buildBtn.SetText("开始构建")
+		m.buildBtn.SetFont(btnFont)
+		m.buildBtn.Font().SetColor(colors.ClWhite)
+		m.buildBtn.SetRadius(3)
+		buildBtnRect := types.TRect{Left: saveBtnRect.Left + saveBtnRect.Width() + 10, Top: saveBtnRect.Top}
+		buildBtnRect.SetWidth(75)
+		buildBtnRect.SetHeight(25)
+		m.buildBtn.SetBoundsRect(buildBtnRect)
+		m.buildBtn.SetColor(colors.RGBToColor(46, 204, 113))
+		m.buildBtn.SetParent(m.buildTab)
+		m.buildBtn.SetOnClick(m.saveClick)
 	}
 	//(&hook.TWindowHook{Form: m}).Hook()
+}
+
+func (m *TBuildForm) initConfigComponent() {
+	titleFont := lcl.NewFont()
+	titleFont.SetName("微软雅黑")
+	titleFont.SetSize(12)
+	titleFont.SetStyle(types.NewSet(types.FsBold))
+
+	titleFontTwo := lcl.NewFont()
+	titleFontTwo.SetName("微软雅黑")
+	titleFontTwo.SetSize(10)
+	titleFontTwo.SetStyle(types.NewSet(types.FsBold))
+
+	targetPlatformTitle := lcl.NewLabel(m)
+	targetPlatformTitle.SetFont(titleFont)
+	targetPlatformTitle.SetCaption("目标平台与架构")
+	targetPlatformTitle.SetTop(5)
+	targetPlatformTitle.SetLeft(10)
+	targetPlatformTitle.SetParent(m.buildTabPageConfig)
+
+	platformTitle := lcl.NewLabel(m)
+	platformTitle.SetCaption("平台")
+	platformTitle.SetLeft(10)
+	platformTitle.SetTop(35)
+	platformTitle.SetFont(titleFontTwo)
+	platformTitle.SetParent(m.buildTabPageConfig)
+}
+
+func (m *TBuildForm) initBuildComponent() {
+
 }
 
 func (m *TBuildForm) OnCloseQuery(sender lcl.IObject, canClose *bool) {
