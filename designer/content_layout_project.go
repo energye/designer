c63b89f092796e79ed506eb3b39a1088831c8385
yanghy <snxamdf@126.com>
2026-04-26 01:23:18 +0800

    新的设计器布局

--- designer/content_layout_project.go
+++ designer/content_layout_project.go
@@ -3,12 +3,19 @@ package designer
 import (
 	"github.com/energye/lcl/lcl"
 	"github.com/energye/lcl/types"
+	"github.com/energye/lcl/types/colors"
 )
 
 type ContentLayoutProject struct {
 	topBox lcl.IPanel
 	title  lcl.ILabel
 	box    lcl.IPanel
+	// 项目栏 tree, 2个根节点 componentRoot assetsRoot
+	tree lcl.ITreeView
+	// 1: 组件根节点(所有窗体和组件)
+	componentRoot lcl.ITreeNode
+	//  2: 资源目录和文件根节点（所有代码和文件）
+	assetsRoot lcl.ITreeNode
 }
 
 func initContentLayoutProject(owner *ContentLayout) *ContentLayoutProject {
@@ -34,6 +41,26 @@ func initContentLayoutProject(owner *ContentLayout) *ContentLayoutProject {
 	m.box.SetDoubleBuffered(true)
 	m.box.SetAlign(types.AlClient)
 	m.box.SetParent(owner.projectPanel)
+
+	m.tree = lcl.NewTreeView(owner.projectPanel)
+	m.tree.SetAutoExpand(true)
+	m.tree.SetReadOnly(true)
+	m.tree.SetDoubleBuffered(true)
+	m.tree.SetTreeLineColor(colors.RGBToColor(128, 128, 128))
+	m.tree.SetTreeLinePenStyle(types.PsSolid)
+	m.tree.SetAlign(types.AlClient)
+	m.tree.SetVisible(false)
+	m.tree.SetBorderStyleToBorderStyle(types.BsNone)
+	m.tree.Font().SetHeight(-10)
+	//form.tree.SetImages(imageComponents.ImageList50())
+	m.tree.SetImages(imageComponents.ImageList100())
+	//m.tree.SetMultiSelect(true) // 多选控制
+	//m.tree.SetOnGetSelectedIndex(form.TreeOnGetSelectedIndex)
+	//m.tree.SetOnMouseDown(form.TreeOnMouseDown)
+	//m.tree.SetOnContextPopup(form.TreeOnContextPopup)
+	//m.CreateComponentMenu()
+	//m.tree.SetPopupMenu(form.componentMenu.treePopupMenu)
+	//m.tree.SetParent(m.box)
 
 	return m
 }
