012a8f87bb2bdd3873ec5c560f15fe5f725feff7
yanghy <snxamdf@126.com>
2026-05-21 15:01:45 +0800

    libname type dev / prod

--- cmd/packager/pkg_linux.go
+++ cmd/packager/pkg_linux.go
@@ -160,13 +160,9 @@ func linuxAutoDeps() (debDeps, rpmDeps string) {
 		rpm []string // /n
 	)
 	if gtk3 {
-		deb = append(deb, "libgtk-3-0 (>= 3.24.24)")
-		deb = append(deb, "libglib2.0-0 (>= 2.66.0)")
-		rpm = append(rpm, "Requires: libgtk-3.so.0"+soSuffix+" >= 3.24.24")
-		rpm = append(rpm, "Requires: libglib-2.0.so.0"+soSuffix+" >= 2.66.0")
+		deb = append(deb, "libgtk-3-0 (>= 3.24.24)", "libglib2.0-0 (>= 2.66.0)")
+		rpm = append(rpm, "Requires: libgtk-3.so.0"+soSuffix+" >= 3.24.24", "Requires: libglib-2.0.so.0"+soSuffix+" >= 2.66.0")
 
-		//debDeps = "libgtk-3-0 (>= 3.24.24), libglib2.0-0 (>= 2.66.0)"
-		//rpmDeps = `Requires: libgtk-3.so.0` + soSuffix + ` >= 3.24.24 Requires: libglib-2.0.so.0` + soSuffix + ` >= 2.66.0`
 		if webkit {
 			// deb := " (>= 2.40.0)"
 			// rpm := " >= 2.40.0"
@@ -174,30 +170,19 @@ func linuxAutoDeps() (debDeps, rpmDeps string) {
 				// CGO + webkit2_4_1 build tag -> 4.1
 				deb = append(deb, "libwebkit2gtk-4.1-0")
 				rpm = append(rpm, "Requires: libwebkit2gtk-4.1.so.0"+soSuffix)
-
-				//debDeps += ", libwebkit2gtk-4.1-0"
-				//rpmDeps += "\nRequires: libwebkit2gtk-4.1.so.0" + soSuffix
 			} else if option.BuildCGOEnabled {
 				// CGO 默认 -> 4.0
 				deb = append(deb, "libwebkit2gtk-4.0-37")
 				rpm = append(rpm, "Requires: libwebkit2gtk-4.1.so.0"+soSuffix)
-
-				//debDeps += ", libwebkit2gtk-4.0-37"
-				//rpmDeps += "\nRequires: libwebkit2gtk-4.0.so.37" + soSuffix
 			} else {
 				// 非 CGO dlopen 自动降级
 				deb = append(deb, "libwebkit2gtk-4.1-0 | libwebkit2gtk-4.0-37")
 				rpm = append(rpm, "Requires: (libwebkit2gtk-4.1.so.0"+soSuffix+" or libwebkit2gtk-4.0.so.37"+soSuffix+")")
-
-				//debDeps += ", libwebkit2gtk-4.1-0 | libwebkit2gtk-4.0-37"
-				//rpmDeps += "\nRequires: (libwebkit2gtk-4.1.so.0" + soSuffix + " or libwebkit2gtk-4.0.so.37" + soSuffix + ")"
 			}
 		}
 	} else {
 		deb = append(deb, "libgtk2.0-0 (>= 2.24.0)")
 		rpm = append(rpm, "Requires: libgtk-2.0.so.0"+soSuffix+" >= 2.24.0")
-		//debDeps = "libgtk2.0-0 (>= 2.24.0)"
-		//rpmDeps = "Requires: libgtk-2.0.so.0" + soSuffix + " >= 2.24.0"
 	}
 	deb = append(deb, "libharfbuzz-gobject0", "libgl1")
 	rpm = append(rpm, "Requires: libharfbuzz-gobject.so.0"+soSuffix, "Requires: libGL.so.1"+soSuffix)
