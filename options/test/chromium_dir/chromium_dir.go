c385287c0c19142e12cf6537910c7f555cc0e2d1
yanghy <snxamdf@126.com>
2026-06-02 17:20:50 +0800

    Add and Fix: CEF for energy

---
+++ options/test/chromium_dir/chromium_dir.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/energye/designer/options"
+	"github.com/energye/designer/options/bean"
+	"github.com/energye/lcl/api"
+	"github.com/energye/lcl/api/libname"
+	"github.com/energye/lcl/lcl"
+)
+
+func main() {
+	bean.GProject = &bean.TProject{}
+	api.SetDebug(true)
+	form := options.TChromiumDirForm{}
+	lcl.Init()
+	lcl.Application.Initialize()
+	lcl.Application.SetMainFormOnTaskBar(true)
+	lcl.Application.SetScaled(true)
+	lcl.Application.NewForms(&form)
+	form.WorkAreaCenter()
+	lcl.Application.Run()
+}
+
+func init() {
+	libname.UseWS = "gtk3" // for linux
+}
